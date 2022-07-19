dfac9f86b1e0df69e36ea3b125451d0f355fbff8
Christopher D. Riewaldt <c.riewaldt@gmail.com>
2022-07-19 10:40:22 -0400

    first

---
+++ LeetPractice.py
@@ -0,0 +1,17 @@
+from re import A
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution():
+    def mergeTwoLists(self, a, b):
+        answer = []
+        
+
+                    
+
+answer = Solution()
+print(answer.mergeTwoLists([1,2,4], [1,3,4]))
---
+++ isSubsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        
+
+
+
+
+answer = Solution()
+print('answer:', answer.isSubsequence(s="abc", t="ahbgdc"))
