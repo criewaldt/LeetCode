266b49f41256e7224212abf466ebdf92b90e00c6
Christopher D. Riewaldt <c.riewaldt@gmail.com>
2022-07-20 17:35:12 -0400

    longestPalendrome

--- longestPalendrome.py
+++ longestPalendrome.py
@@ -6,22 +6,43 @@ return the length of the longest palindrome that can be built with those letters
 class Solution():
     def longestPalindrome(self, s):
         sMap = {}
-        longest_odd = 0
-        even_count = 0
+        biggest_odd = 0
+        biggest_key = None
+        count = 0
         for char in s:
             if char not in sMap:
                 sMap[char] = 1
             else:
                 sMap[char] += 1
-        for v in sMap.values():
-            if v % 2 != 0:
-                if v > longest_odd:
-                    longest_odd = v
+        
+        for key in sMap.keys():
+            if sMap[key] > biggest_odd:
+                biggest_odd = sMap[key]
+                biggest_key = key
+                
+        count += sMap[biggest_key]
+        del sMap[biggest_key]
+
+        for value in sMap.values():
+            if (value % 2) == 0:
+                count += value
             else:
-                even_count += v
-        return even_count + longest_odd
+                count += value-1
+        return count
+
+        
+
+
+        
+
+
+
 
         
 
 answer = Solution()
-print(answer.longestPalindrome('abccccdd'))
+#print(answer.longestPalindrome('abccccdd'))
+
+test = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+
+print(answer.longestPalindrome(test))
